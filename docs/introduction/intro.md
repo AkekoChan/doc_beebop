3c516630b8d02977361c3a495c4989c51ce9f5e0
AkekoChan <artapp.theo@gmail.com>
2023-04-06 17:50:48 +0200

    start introduction

---
+++ docs/introduction/intro.md
@@ -0,0 +1,11 @@
+# Introduction
+
+Bienvenue dans l'application **Beebop**, conçue pour les apiculteurs qui cherchent à gérer efficacement leurs ruches et leur production de miel. Grâce à notre application, vous pourrez facilement suivre l'état de vos colonies, surveiller les performances de vos ruches, suivre les récoltes de miel et bien plus encore.
+
+Notre application est conçue pour être **simple** et **intuitive** à utiliser, en offrant des fonctionnalités pratiques qui vous permettront de gérer votre activité apicole de manière efficace. Nous avons choisi le framework **PHP Symfony** pour notre application, qui offre une solide base pour créer une application web performante et sécurisée.
+
+Dans cette documentation, vous trouverez toutes les informations nécessaires pour vous aider à utiliser l'application au mieux de ses capacités. Nous espérons que vous trouverez notre application utile et que cette documentation répondra à toutes vos questions.
+
+**Prêt à commencer ? Allons-y !**
+
+[Allez vers Beebop](https://ratio.fr)
--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,43 +1,45 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './styles.module.css';
+import React from "react";
+import clsx from "clsx";
+import styles from "./styles.module.css";
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: "Facile à utiliser",
+    Svg: require("@site/static/img/happy.svg").default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Notre documentation est simple, claire et facile à naviguer pour vous
+        aider à profiter rapidement de notre application
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: "Répondre aux problème",
+    Svg: require("@site/static/img/issues.svg").default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Notre documentation répond aux problèmes courants rencontrés par les
+        utilisateurs, en fournissant des instructions détaillées pour vous aider
+        à résoudre ces problèmes rapidement et facilement.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: "Utilisation de Symfony",
+    Svg: require("@site/static/img/symfony.svg").default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Notre application utilise le framework PHP Symfony, qui est reconnu pour
+        sa robustesse et sa flexibilité. Cela nous permet de fournir une
+        application stable et performante pour répondre à vos besoins
       </>
     ),
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({ Svg, title, description }) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--4")}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
