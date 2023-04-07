57e7b46b9d9e41033dea403d1fe6bd329d4b3828
AkekoChan <artapp.theo@gmail.com>
2023-04-07 09:57:26 +0200

    fix path error in build

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,113 +6,114 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-    title: "Beebop - Documentation",
-    tagline:
-        "La documentation que vous avez besoin pour comprendre l'application Beebop",
-    favicon: "img/icon.svg",
+  title: "Beebop - Documentation",
+  tagline:
+    "La documentation que vous avez besoin pour comprendre l'application Beebop",
+  favicon: "img/icon.svg",
 
-    // Set the production url of your site here
-    url: "https://your-docusaurus-test-site.com",
-    // Set the /<baseUrl>/ pathname under which your site is served
-    // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: "/",
-    staticDirectories: ["public", "static"],
+  // Set the production url of your site here
+  url: "https://your-docusaurus-test-site.com",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+  staticDirectories: ["public", "static"],
 
-    // GitHub pages deployment config.
-    // If you aren't using GitHub pages, you don't need these.
-    organizationName: "facebook", // Usually your GitHub org/user name.
-    projectName: "docusaurus", // Usually your repo name.
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
 
-    onBrokenLinks: "throw",
-    onBrokenMarkdownLinks: "warn",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
-    // Even if you don't use internalization, you can use this field to set useful
-    // metadata like html lang. For example, if your site is Chinese, you may want
-    // to replace "en" with "zh-Hans".
-    i18n: {
-        defaultLocale: "fr",
-        locales: ["fr"],
-    },
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "fr",
+    locales: ["fr"],
+  },
 
-    presets: [
-        [
-            "classic",
-            /** @type {import('@docusaurus/preset-classic').Options} */
-            ({
-                docs: {
-                    sidebarPath: require.resolve("./sidebars.js"),
-                    // Please change this to your repo.
-                    // Remove this to remove the "edit this page" links.
-                    editUrl:
-                        "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-                },
-                blog: {
-                    showReadingTime: true,
-                    // Please change this to your repo.
-                    // Remove this to remove the "edit this page" links.
-                    editUrl:
-                        "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-                },
-                theme: {
-                    customCss: require.resolve("./src/css/custom.css"),
-                },
-            }),
-        ],
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: "/",
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        },
+        blog: {
+          showReadingTime: false,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
     ],
+  ],
 
-    themeConfig:
-        /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-        ({
-            navbar: {
-                title: "Beebop",
-                logo: {
-                    alt: "Logo Beebop",
-                    src: "img/logo.svg",
-                    href: "/",
-                },
-                items: [
-                    {
-                        type: "docSidebar",
-                        sidebarId: "tutorialSidebar",
-                        position: "left",
-                        label: "Documentation",
-                    },
-                    {
-                        href: "https://github.com/mchauveni/beebop_app",
-                        label: "GitHub",
-                        position: "right",
-                    },
-                    {
-                        href: "#",
-                        label: "Beebop",
-                        position: "right",
-                    },
-                ],
-            },
-            footer: {
-                style: "dark",
-                links: [
-                    {
-                        label: "Découvrir Beebop",
-                        to: "/docs/intro",
-                    },
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "Beebop",
+        logo: {
+          alt: "Logo Beebop",
+          src: "img/logo.svg",
+          href: "/",
+        },
+        items: [
+          {
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "Documentation",
+          },
+          {
+            href: "https://github.com/mchauveni/beebop_app",
+            label: "GitHub",
+            position: "right",
+          },
+          {
+            href: "#",
+            label: "Beebop",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            label: "Découvrir Beebop",
+            to: "/docs/intro",
+          },
 
-                    {
-                        label: "GitHub",
-                        href: "https://github.com/mchauveni/beebop_app",
-                    },
-                    {
-                        label: "Beebop",
-                        href: "#",
-                    },
-                ],
-                copyright: `Copyright © ${new Date().getFullYear()} Beebop`,
-            },
-            prism: {
-                theme: lightCodeTheme,
-                darkTheme: darkCodeTheme,
-            },
-        }),
+          {
+            label: "GitHub",
+            href: "https://github.com/mchauveni/beebop_app",
+          },
+          {
+            label: "Beebop",
+            href: "#",
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Beebop`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
 };
 
 module.exports = config;
