ed19eb478fbbe70eefdba9534f802d3933fbdd99
AkekoChan <artapp.theo@gmail.com>
2023-04-07 10:07:29 +0200

    fix upload path

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,9 +12,7 @@ const config = {
   favicon: "img/icon.svg",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-test-site.com",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
+  url: "https://docusaurus-2.netlify.app", // Url to your site with no trailing slash
   baseUrl: "/",
   staticDirectories: ["public", "static"],
 
